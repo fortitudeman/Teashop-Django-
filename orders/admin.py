b968b90735b5f3d5823da3b1771e5d1545f454d1
flynn-rider-expert <xiaoping@stlpro.com>
2020-08-12 20:10:33 -0500

    rendering pdf files using weasyprint

--- orders/admin.py
+++ orders/admin.py
@@ -36,6 +36,12 @@ def export_to_csv(modeladmin, request, queryset):
 
 export_to_csv.short_description = 'Export to CSV'
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+order_pdf.short_description = 'Invoice'
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -46,7 +52,7 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
         'address', 'postal_code', 'city', 'paid', 'braintree_id',
-        'created', 'updated', order_detail]
+        'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
